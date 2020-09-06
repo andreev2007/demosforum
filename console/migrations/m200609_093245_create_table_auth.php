31718645ee7d7134fdade50d14c2b95ed63f05d1
Andreev Ilya <andreevdaniil564@gmail.com>
2020-09-06 09:35:11 +0200

    Added Auth model and migration

---
+++ console/migrations/m200609_093245_create_table_auth.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m200609_093245_create_table_auth extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('auth', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'source' => $this->string()->notNull(),
+            'source_id' => $this->string()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%auth}}');
+    }
+
+}
