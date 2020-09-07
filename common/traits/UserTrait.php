38a787c1e8b366e59e8eb96d8e8dc543c114ea3d
Andreev Ilya <andreevdaniil564@gmail.com>
2020-09-07 12:37:51 +0200

    Delete username from signup form

--- common/traits/UserTrait.php
+++ common/traits/UserTrait.php
@@ -9,7 +9,7 @@ trait UserTrait
     public function getName()
     {
         if(!Yii::$app->user->isGuest) {
-            $username = \Yii::$app->user->identity->first_name . \Yii::$app->user->identity->last_name;
+            $username = \Yii::$app->user->identity->first_name .' '. \Yii::$app->user->identity->last_name;
 
         } else {
             $username = null;
